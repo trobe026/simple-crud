8c1ec7bcead5c972fe4b4d04ab463df183553a68
Travis Roberts <travis@datafiniti.co>
2021-05-16 18:37:10 -0600

    move schemas

--- src/index.js
+++ src/index.js
@@ -1,27 +1,80 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
-const PORT = 3000;
-
-// try handlebars for dynamic html
-const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-// serve static files
-app.use(express.static('public'));
-
-require('./routes/home.js')(app);
-require('./routes/modify.js')(app);
+const Schema = mongoose.Schema;
 
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect('mongo:27017');
+mongoose.connect('mongodb://mongo:27017');
 
+const TaskSchema = new Schema({
+    title: {
+        type: String
+    },
+    text: {
+        type: String,
+        required: [true, 'A task is required']
+    }
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+const UserSchema = new Schema({
+    user: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: [true, 'A password is required']
+    },
+    tasks: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
+});
+
+const User = mongoose.model('User', UserSchema);
+
+const app = express();
+app.use(express.json());
+const PORT = 9000;
 app.listen(PORT, function() {
     console.log('App now listening at localhost:' + PORT);
 });
 
-app.get('/', function (req, res) {
+// health check
+app.get('/health', function (req, res) {
     res.send('hello world!');
+});
+
+// get tasks or task
+app.get('/tasks', async function(req, res) {
+    let tasks = await Task.find({});
+    res.json(tasks);
+});
+
+app.get('/tasks/:id', async function(req, res) {
+    let task = await Task.findById({ _id: req.params.id });
+    res.json(task);
+});
+
+// create a task and link to the user
+app.post('/tasks/:id', async function(req, res) {
+    let { body } = req;
+    let task = await Task.create(body).then(() => User.findOneAndUpdate({ _id: req.params.id }));
+    res.json(task);
+});
+
+// update a task
+app.put('/tasks/:id', async function(req, res) {
+    let { body } = req;
+    let task = await Task.findByIdAndUpdate( { _id: req.params.id, body });
+    res.json(task);
+});
+
+// delete a task
+app.delete('/tasks/:id', async function(req, res) {
+    let task = await Task.deleteOne({ _id: req.params.id });
+    res.json(task);
 });
