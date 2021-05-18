9935cd0b616c65da333d03566ae7237c1e94a854
Travis Roberts <travis@datafiniti.co>
2021-05-18 14:46:34 -0600

    move

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,48 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
 const UserSchema = new Schema({
-    _id: {
-        type: Number
-    },
-    username: {
+    user: {
         type: String,
-        required: [true]
+        required: true,
+        index: {
+            unique: true
+        }
     },
     password: {
         type: String,
-        required: [true]
-    }
+        required: [true, 'A password is required']
+    },
+    tasks: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
+});
+
+UserSchema.pre('save', function saveHook(next) {
+    const user = this;
+
+    // check if the password is modified or the user is new
+    if (!user.isModified('password')) return next();
+
+
+    return bcrypt.genSalt((saltError, salt) => {
+        if (saltError) { return next(saltError); }
+
+        return bcrypt.hash(user.password, salt, (hashError, hash) => {
+            if (hashError) { return next(hashError); }
+
+            // replace a password string with hash value
+            user.password = hash;
+
+            return next();
+        });
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
