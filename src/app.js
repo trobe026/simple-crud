4a73c8aec948e0dc0289cdb28e9f9558775542c7
Travis Roberts <travis@datafiniti.co>
2021-05-19 09:57:44 -0600

    add router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const healthRouter = require('./routes/health');
+const taskRouter = require('./routes/tasks');
+const userRouter = require('./routes/users');
 
 const app = express();
+
 app.use(express.json());
+
+const mainRouter = express.Router();
+
+mainRouter.use('/health', healthRouter);
+mainRouter.use('/tasks', taskRouter);
+mainRouter.use('/users', userRouter);
+
+app.use(mainRouter);
 
 module.exports = app;
