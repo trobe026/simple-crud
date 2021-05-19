433911e1df14881e6a5c633d11c1c012da9ba219
Travis Roberts <travis@datafiniti.co>
2021-05-18 19:02:50 -0600

    finish task route tests

--- test/task.js
+++ test/task.js
@@ -1,31 +1,85 @@
 const mongoose = require('mongoose');
 
 const chai = require('chai');
-const chaiHttp = require('chai-http');
+const expect = chai.expect;
 const should = chai.should();
 
-const Task = require('../src/models/Task');
-
+const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-
-mongoose.Promise = Promise;
-mongoose.connect('mongodb://mongo:27017');
 
 describe('Tasks', () => {
 
+    const host = 'localhost:9000';
+    const task_id = 1;
+
     before(async() => {
-        mongoose.connect('mongodb://mongo:27017');
+
+        await chai.request(host)
+            .delete('/tasks/');
     });
 
-    describe('/GET tasks', () => {
+    describe('GET and POST tasks', () => {
+
         it('Should get all tasks', async() => {
-            chai.request('localhost:9000')
-                .get('/tasks')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
-                    res.body.length.should.be.eql(0);
-                });
+
+            let resp = await chai.request(host).get('/tasks');
+
+            resp.should.have.status(200);
+            resp.body.should.be.a('array');
+            resp.body.length.should.be.eql(0);
+        });
+
+        it('Should create a new task', async() => {
+
+            const task = {
+                title: 'test task',
+                text: 'some to-do'
+            };
+
+            let resp = await chai.request(host).post('/tasks/' + task_id).send(task);
+
+            expect(resp.status).to.equal(200);
+            resp.body.should.be.a('object');
+            expect(resp.body.title).to.equal('test task');
+            expect(resp.body.text).to.equal('some to-do');
+        });
+
+        it('Should get a task', async() => {
+
+            let resp = await chai.request(host).get('/tasks/' + task_id);
+
+            resp.should.have.status(200);
+            resp.body.should.be.a('object');
+            expect(resp.body.title).to.equal('test task');
+            expect(resp.body.text).to.equal('some to-do');
+        });
+    });
+
+    describe('PUT and DEL tasks', () => {
+
+        it('Should get update a task', async() => {
+
+            const newTask = {
+                title: 'test task',
+                text: 'updated task'
+            };
+
+            let resp = await chai.request(host).put('/tasks/' + task_id).send(newTask);
+
+            expect(resp.status).to.equal(200);
+            resp.body.should.be.a('object');
+            expect(resp.body.title).to.equal('test task');
+            expect(resp.body.text).to.equal('updated task');
+        });
+
+        it('Should delete a task', async() => {
+
+            let resp = await chai.request(host).del('/tasks/' + task_id);
+
+            expect(resp.status).to.equal(200);
+            resp.body.should.be.a('object');
+            expect(resp.body.ok).to.equal(1);
+            expect(resp.body.deletedCount).to.equal(1);
         });
     });
 
