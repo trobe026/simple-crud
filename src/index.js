f8289e8a26c66eda69aaf2244956eb69fc458bbb
Travis Roberts <travis@datafiniti.co>
2021-05-19 09:57:56 -0600

    move routes to routes dir

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-const app = require('./app');
-
-const Task = require('./models/Task');
-const User = require('./models/User');
-
 const mongoose = require('mongoose');
-const passport = require('passport');
+const app = require('./app');
 
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
@@ -14,87 +9,5 @@ mongoose.connect('mongodb://mongo:27017', { useNewUrlParser: true, useUnifiedTop
 const PORT = 9000;
 app.listen(PORT, function() {
     console.log('App now listening at localhost:' + PORT);
-});
-
-
-// TASK ROUTES
-
-// health check
-app.get('/health', function (req, res) {
-    res.send('hello world!');
-});
-
-// get tasks or task
-app.get('/tasks', async function(req, res) {
-    let tasks = await Task.find({});
-    res.json(tasks);
-});
-
-app.get('/tasks/:id', async function(req, res) {
-    let task = await Task.findById({ _id: req.params.id });
-    if (!task) {
-        res.json({ error: 'no task exists for id: ' + req.params.id });
-    }
-    res.json(task);
-});
-
-// create a task and link to the user
-app.post('/tasks/:id', async function(req, res) {
-    let { body } = req;
-    let task = await Task.create( { _id: req.params.id, title: body.title, text: body.text });
-    res.json(task);
-});
-
-// update a task
-app.put('/tasks/:id', async function(req, res) {
-    let { body } = req;
-    let task = await Task.findByIdAndUpdate( { _id: req.params.id }, body, { useFindAndModify: false, new: true });
-    res.json(task);
-});
-
-// delete a task
-app.delete('/tasks/:id', async function(req, res) {
-    let task = await Task.deleteOne({ _id: req.params.id });
-    res.json(task);
-});
-
-// delete all tasks
-app.delete('/tasks/', async function(req, res) {
-    let task = await Task.deleteMany({});
-    res.json(task);
-});
-
-// USER ROUTES
-
-// create a user
-app.get('/signup', async function (req, res) {
-    let { body } = req;
-    let user = await User.create( { _id: req.params.id }, body);
-    res.json(user);
-});
-
-// update user
-app.put('/users', async function(req, res) {
-    let { body } = req;
-    let user = await User.findByIdAndUpdate( { _id: req.params.id }, body, { new: true });
-    res.json(user);
-});
-
-// get user
-app.get('/users', async function(req, res) {
-    let user = await User.findById( { _id: req.params.id });
-    res.json(user);
-});
-
-// get users
-app.get('/users', async function(req, res) {
-    let user = await User.find({});
-    res.json(user);
-});
-
-// delete user
-app.delete('/users', async function(req, res) {
-    let user = await User.deleteOne( { _id: req.params.id });
-    res.json(user);
 });
 
