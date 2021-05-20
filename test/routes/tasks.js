698ab89b746ed1cb7a865c179466a94b3bcccb77
Travis Roberts <travis@datafiniti.co>
2021-05-20 09:53:08 -0600

    Create tasks.js

---
+++ test/routes/tasks.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+
+const { createTask } = require('../utils');
+
+describe('Tasks API', () => {
+
+    describe('/GET tasks', () => {
+
+        it('should get a task', async() => {
+
+        });
+
+        it('should get all tasks', async() => {
+
+        });
+    });
+
+    describe('/POST tasks', () => {
+
+        it('should create a task', async() => {
+
+        });
+    });
+
+    describe('/PUT tasks', () => {
+
+        it('should update a task', async() => {
+
+        });
+    });
+
+    describe('/DEL tasks', () => {
+
+        it('should delete a task', async() => {
+
+        });
+    });
+
+
+
+});
